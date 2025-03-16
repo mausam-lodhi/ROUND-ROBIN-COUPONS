feaa89432de2e1c21faa53185a4451badc5d60d5
mausam-lodhi <harprasadlodhi1984@gmail.com>
2025-03-16 21:35:35 +0530

    Update HomePage.jsx

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export default function HomePage() {
 	return (
 		<div className='min-h-screen bg-gradient-to-br from-indigo-50 via-white to-cyan-50 flex items-center justify-center px-4 py-8 p-20'>
-			<div className='glass-card animate-fade-up p-6 md:p-10 max-w-4xl w-full mx-auto rounded-2xl '>
+			<div >
 				<h1>Coupon Management System</h1>
 				<br />
 				<br />
